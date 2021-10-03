6eb1d7d5179481032de6c1b5584be631908e233c
muhammad-ahsan9776 <muhammad.ahsan9776@gmail.com>
2021-10-03 19:16:18 +0500

    Wrote simulateVariationOfSum4 and simulateVariationOfSum5

--- EAD_P1_VariationsOfSum/Program.cs
+++ EAD_P1_VariationsOfSum/Program.cs
@@ -10,14 +10,10 @@ namespace EAD_P1_VariationsOfSum
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter Operand 1: ");
-            int operand1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Operand 2: ");
-            int operand2 = Convert.ToInt32(Console.ReadLine());
-            simulateSumVariation3(operand1,operand2);
+            simulateSumVariation5();
             Console.ReadKey();
         }
-        static void simulateSumVaritarion1()
+        static void simulateSumVariation1()
         {
             Console.Write("Enter Operand 1: ");
             int operand1 = Convert.ToInt32(Console.ReadLine());
@@ -25,7 +21,7 @@ namespace EAD_P1_VariationsOfSum
             int operand2 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Sum Variation 1: " + (operand1 + operand2));
         }
-        static void simulateSumVaritarion2()
+        static void simulateSumVariation2()
         {
             Console.Write("Enter Operand 1: ");
             int operand1 = Convert.ToInt32(Console.ReadLine());
@@ -36,6 +32,18 @@ namespace EAD_P1_VariationsOfSum
         static void simulateSumVariation3(int operand1,int operand2)
         {
             Console.Write("Sum Variation 3: " + getSum(operand1, operand2));
+        }
+        static void simulateSumVariation4(int operand1, int operand2)
+        {
+            Console.Write("Sum Variation 4: " + (operand1 + operand2));
+        }
+        static void simulateSumVariation5()
+        {
+            Console.Write("Enter Operand 1: ");
+            double operand1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Operand 2: ");
+            double operand2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sum Variation 5: " + (operand1 + operand2));
         }
         static int getSum(int a, int b)
         {
