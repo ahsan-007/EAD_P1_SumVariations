4875cdee71659439b64ae6bf1b98fcb5a272c453
muhammad-ahsan9776 <muhammad.ahsan9776@gmail.com>
2021-10-03 19:09:49 +0500

    Wrote simulateVariationOfSum2

--- EAD_P1_VariationsOfSum/Program.cs
+++ EAD_P1_VariationsOfSum/Program.cs
@@ -10,7 +10,7 @@ namespace EAD_P1_VariationsOfSum
     {
         static void Main(string[] args)
         {
-            simulateSumVaritarion1();
+            simulateSumVaritarion2();
             Console.ReadKey();
         }
         static void simulateSumVaritarion1()
@@ -20,6 +20,18 @@ namespace EAD_P1_VariationsOfSum
             Console.Write("Enter Operand 2: ");
             int operand2 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Sum Variation 1: " + (operand1 + operand2));
+        }
+        static void simulateSumVaritarion2()
+        {
+            Console.Write("Enter Operand 1: ");
+            int operand1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Operand 2: ");
+            int operand2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Sum Variation 1: " + getSum(operand1,operand2));
+        }
+        static int getSum(int a, int b)
+        {
+            return a + b;
         }
     }
 }
